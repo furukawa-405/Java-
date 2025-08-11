0c21b15d26384cf241ab10de05c2bb38f39b6f94
shing <shing@192.168.3.6>
2025-08-12 00:04:32 +0900

    提出 課題Qes1_問2

---
+++ helloWorld_furukawashingo/src/curriculum_A/Qes1.java
@@ -0,0 +1,23 @@
+package curriculum_A;
+
+public class Qes1 {
+
+	public static void main(String[] args) {
+		// 問１
+		byte byteNum = 0;
+		short shortNum = 0;
+		int intNum = 0;
+		long longNum = 0;
+		double doubleNum = 0.0;
+		char letter = '\u0000';
+		String letters = null;
+		boolean isBoolean = false;
+		
+		//問２
+		byteNum = 10;
+		shortNum = 100;
+		intNum = 1000;
+
+	}
+
+}
