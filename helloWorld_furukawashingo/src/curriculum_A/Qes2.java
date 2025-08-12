c0b7f1a30b466f4968f826baf7bb683aea0ec90d
shing <shing@192.168.3.6>
2025-08-12 13:43:10 +0900

    提出 課題Qes2_問3～7

---
+++ helloWorld_furukawashingo/src/curriculum_A/Qes2.java
@@ -0,0 +1,55 @@
+package curriculum_A;
+
+public class Qes2 {
+
+	public static void main(String[] args) {
+	//問3
+	short x = 11110;
+	System.out.println(x);
+	int y = 20;
+	System.out.println(y);
+	char letter = 'a';
+	String letters = "ハロー";
+	boolean isBoolean = true;
+	System.out.println(letter+letters+isBoolean);
+	System.out.println(x+y);
+	System.out.println(x*y);
+	double b = 10.5;
+	int c = 100;
+	System.out.println(b/c);
+	int d = 10;
+	int e = 100;
+	System.out.println(d-e);
+	//問4
+	String name = "山田太郎";
+	String greeting = "こんにちは、";
+	String mark = "さん！";
+	System.out.println(greeting+name+mark);
+	//問5
+	int age = 25;
+	String f = "年齢:";
+	char g = '歳';
+	System.out.println(f+age+g);
+	//問6
+	int num1 = 10;
+	int num2 = 5;
+	int sum = num1 + num2;
+	System.out.println(sum);
+	//問7
+	int score = 80;
+	score += 20;
+	System.out.println(score);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
