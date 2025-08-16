04f996881eb79a0b24ea0ed1e499df2c37106881
shing <shing@192.168.3.6>
2025-08-16 22:33:54 +0900

    提出:curriculum_B完了しました

--- helloWorld_furukawashingo/src/curriculum_B/Qes2.java
+++ helloWorld_furukawashingo/src/curriculum_B/Qes2.java
@@ -54,7 +54,53 @@ public class Qes2 {
     String a = "";
     if (a == ""){System.out.println("入力が無効です");}
 	
+    //問9
+    int day = 1;
+    switch (day) {
+    case 1:System.out.println("今日は月曜日です");
+    break;
+    case 2:System.out.println("今日は火曜日です");
+    break;
+    case 3:System.out.println("今日は水曜日です");
+    break;
+    case 4:System.out.println("今日は木曜日です");
+    break;
+    case 5:System.out.println("今日は金曜日です");
+    break;
+    case 6:System.out.println("今日は土曜日です");
+    break;
+    case 7:System.out.println("今日は日曜日です");
+    break;
+    default:System.out.println("入力無効な数字です");
+    break;}
     
+    //問10
+    int month = 1; 
+    switch (month) { case 1: System.out.println("冬");
+    break;
+    case 2 : System.out.println("冬");
+    break;
+    case 12: System.out.println("冬");
+    break;
+    case 3: System.out.println("春"); 
+    break; 
+    case 4: System.out.println("春"); 
+    break; 
+    case 5: System.out.println("春"); 
+    break; 
+    case 6: System.out.println("夏"); 
+    break; 
+    case 7: System.out.println("夏");
+    break; 
+    case 8: System.out.println("夏"); 
+    break; 
+    case 9: System.out.println("秋"); 
+    break; 
+    case 10: System.out.println("秋"); 
+    break; 
+    case 11: System.out.println("秋"); 
+    break;
+    default: System.out.println("無効な月です");}
 	
 	
 	}
