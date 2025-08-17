1ee81e7aedbce995dffaecda79f66088c984b4db
shing <shing@192.168.3.6>
2025-08-17 23:27:57 +0900

    提出:curriculum_B 問5まで完了

---
+++ helloWorld_furukawashingo/src/curriculum_B/Qes3.java
@@ -0,0 +1,32 @@
+package curriculum_B;
+
+public class Qes3 {
+
+	public static void main(String[] args) {
+
+	//問1
+	for (int i = 1; i <= 10; i++){System.out.println(i);}
+    
+	//問2
+	for (int a = 2; a <= 20; a++)
+	if (a % 2 == 0){System.out.println(a);}
+	
+	//問3
+	for (int b = 10; b >= 1; b--){System.out.println("カウントダウン:" + b);}
+    
+	//問4
+	int c = 0;
+	for (int d = 1; d <= 100; d++) {c += d;}System.out.println(c);
+	
+	//問5
+	String[] a = { "*", "**", "***", "****", "*****" };
+	for (String b : a) {System.out.println(b);}
+	
+	//問6
+	
+	
+	
+	
+	}
+
+}
